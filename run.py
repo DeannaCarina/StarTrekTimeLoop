b29ed052dd6be30c481a38e7cd0f00838c979d22
DeannaCarina <deannacarina@hotmail.com>
2021-08-16 14:31:14 +0000

    add: Function names to run.py

--- run.py
+++ run.py
@@ -1,1 +1,39 @@
-# python code goes here
+health = 5
+inventory = []
+name = None
+
+def Playgame()
+def EndGame()
+
+# Starting room
+def EngineBay()
+
+#Ending room
+def TransporterRoom()
+
+# Rooms around the outside of the engine bay
+def Room1.1N() #Health -3
+def Room1.1E() #Health -2
+def Room1.1S() #Health -1
+def Room1.1W() #Health -2
+def Room1.1NE() #Health +1
+def Room1.1SE() #Health +1
+def Room1.1SW() #Health +1
+def Room1.1NW() #Health +1
+
+#Rooms around the outside of the map (not including transporter room)
+def Room1.2N() #kill room
+def Room1.2E() #Health -1
+def Room1.2S() #Health -2
+def Room1.2W() #Health -1
+def Room1.2NE() #Kill Room
+def Room1.2SE() #Health +3
+def Room1.2SW() #Health +2
+def Room1.2NW() #Health +3
+def RoomNNE() #Knife weapon
+def RoomENE() #Locator device
+def RoomSSE() #Batteries
+def RoomSSW() #Transporter key
+def RoomWSW() #kill room
+def RoomWNW() #Phaser Weapon
+def RoomNNW() #Comms device
